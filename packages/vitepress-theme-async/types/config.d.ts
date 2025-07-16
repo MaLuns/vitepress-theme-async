183a11a6826639b597393f2098823aa8ec524846
白云苍狗 <31614024+MaLuns@users.noreply.github.com>
2025-07-16 08:45:15 +0000

    ✨ feat: 新增 config.d.ts 文件

---
+++ packages/vitepress-theme-async/types/config.d.ts
@@ -0,0 +1,6 @@
+import { UserConfig } from 'vitepress';
+import './theme.d.ts';
+
+export function defineConfig(config: UserConfig<AsyncThemeConfig>): UserConfig<AsyncThemeConfig>;
+
+export const defaultConfig: UserConfig<AsyncThemeConfig>;
